1fd06b1b2d637265342558beae2173baeec97913
viduah <developer5@appletechlabs.com>
2024-07-24 23:22:32 +0530

    Form validationn

--- src/layouts/classes/CreateClass.js
+++ src/layouts/classes/CreateClass.js
@@ -13,11 +13,52 @@ import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 // Data
 import authorsTableData from "layouts/tables/data/authorsTableData";
 import projectsTableData from "layouts/tables/data/projectsTableData";
-import { Button, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { Button, FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
 import MDTypography from "components/MDTypography";
 import MDInput from "components/MDInput";
+import { useState } from "react";
+import { Password } from "@mui/icons-material";
 
 function CreateClass() {
+  const [values, setValues] = useState({
+    className: "",
+    classTeacher: "",
+  });
+
+  const [errors, setErrors] = useState({
+    className: "",
+    classTeacher: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const validate = () => {
+    let newErrors = { ...errors };
+    let isValid = true;
+    for (let key in values) {
+      if (values[key] === "") {
+        newErrors[key] = "This " + key + " field is required";
+        isValid = false;
+      } else {
+        newErrors[key] = "";
+      }
+    }
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validate()) {
+      console.log(values);
+    } else {
+      console.log("Error", errors);
+    }
+  };
+
   return (
     <DashboardLayout>
       <DashboardNavbar />
@@ -44,23 +85,49 @@ function CreateClass() {
                 <MDBox component="form" role="form">
                   <MDBox display="flex" justifyContent="center" alignItems="center">
                     <MDBox m={1} pb={2} width={"25%"}>
-                      <MDInput type="text" label="Class Name" fullWidth></MDInput>
+                      <MDInput
+                        error={!!errors.className}
+                        type="text"
+                        label="Class Name"
+                        name="className"
+                        fullWidth
+                        value={values.className}
+                        onChange={handleChange}
+                      />
                     </MDBox>
                     <MDBox m={1} pb={2} width={"25%"}>
-                      <FormControl fullWidth variant="outlined">
+                      <FormControl fullWidth variant="outlined" error={!!errors.classTeacher}>
                         <InputLabel id="teacher-select-label">Select class teacher</InputLabel>
                         <Select
                           labelId="teacher-select-label"
                           id="techer-select"
+                          name="classTeacher"
                           label="Select class teacher"
                           sx={{ height: "44px;" }}
+                          value={values.classTeacher}
+                          onChange={handleChange}
                         >
-                          <MenuItem value={10}>Ten</MenuItem>
-                          <MenuItem value={20}>Twenty</MenuItem>
-                          <MenuItem value={30}>Thirty</MenuItem>
+                          <MenuItem value={1}>Umayanga Vidunwan</MenuItem>
+                          <MenuItem value={2}>Umayanga Vidunwan</MenuItem>
+                          <MenuItem value={3}>Umayanga Vidunwan</MenuItem>
                         </Select>
                       </FormControl>
                     </MDBox>
+                  </MDBox>
+                  <MDBox m={1} pb={2} display="flex" justifyContent="end">
+                    <Button
+                      variant="contained"
+                      sx={{
+                        backgroundColor: "#4caf50", // custom background color
+                        color: "#ffffff", // custom text color
+                        "&:hover": {
+                          backgroundColor: "#388e3c", // custom hover color
+                        },
+                      }}
+                      onClick={handleSubmit}
+                    >
+                      Save
+                    </Button>
                   </MDBox>
                 </MDBox>
               </MDBox>
