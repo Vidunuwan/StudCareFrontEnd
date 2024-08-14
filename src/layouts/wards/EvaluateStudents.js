86d45f6b945c604e4aff3703802a565fe518fbd7
viduah <developer5@appletechlabs.com>
2024-08-14 10:57:20 +0530

    Evaluate update

--- src/layouts/wards/EvaluateStudents.js
+++ src/layouts/wards/EvaluateStudents.js
@@ -26,31 +26,34 @@ import Swal from 'sweetalert2'
 
 function EvaluateStudents() {
     const [values, setValues] = useState({
+        evaluationMonth: "",
+        evaluationYear: "",
+        behavioralData: "",
+        extraNote: "",
+        extracurricularActivities: "",
+        healthData: "",
         wardName: "",
-        hostelMaster: {
-            email: ""
-        },
+        sportData: "",
+        extracurricularActivityGrade: "",
+        sportGrade: ""
     });
 
     const [errors, setErrors] = useState({
+        evaluationMonth: "",
+        evaluationYear: "",
+        behavioralData: "",
+        extraNote: "",
+        extracurricularActivities: "",
+        healthData: "",
         wardName: "",
-        hostelMaster: {
-            email: ""
-        },
+        sportData: "",
+        extracurricularActivityGrade: "",
+        sportGrade: ""
     });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        if (name === "hostelMaster") {
-            setValues({
-                ...values,
-                hostelMaster: {
-                    email: value,
-                },
-            });
-        } else {
-            setValues({ ...values, [name]: value });
-        }
+        setValues({ ...values, [name]: value });
     };
 
     const validate = () => {
@@ -76,13 +79,17 @@ function EvaluateStudents() {
         })
     };
 
+    const [studentEmail, setStudentEmail] = useState();
+
+    const authUser = JSON.parse(localStorage.getItem('authUser'));
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         if (validate()) {
             const formData = values;
             console.log(formData);
             try {
-                const response = await api.post(API_ENDPOINTS.CREATE_WARD, formData);
+                const response = await api.post(`/hostelmaster/${authUser.email}/student/${studentEmail}/evaluation`, formData);
                 const data = response.data;
                 showAlert();
                 setValues({
@@ -110,21 +117,56 @@ function EvaluateStudents() {
             const response = await api.get(`${API_ENDPOINTS.GET_USERS}?role=HOSTEL_MASTER`);
             const teachers = response.data;
             return teachers.data;
-
         } catch (error) {
             return [];
         }
     };
 
+    const getWards = async () => {
+        try {
+            const response = await api.get(`hostelmaster/${authUser.email}/ward`);
+            return response.data.data;
+
+        } catch (error) {
+            console.log("Error", error);
+            return [];
+        }
+
+    }
+
+    const [wardDetails, setWardDetails] = useState();
+
+    const getWardsDetails = async () => {
+        try {
+            const response = await api.get(`ward/${values.wardName}/details`);
+            console.log(response.data.data);
+            return response.data.data;
+
+        } catch (error) {
+            console.log("Error", error);
+            return [];
+        }
+    }
+
+
     const [items, setItems] = useState([]);
 
     useEffect(() => {
         async function fetchItems() {
-            const wardens = await getWardens();
-            setItems(wardens);
+            const wards = await getWards();
+            setItems(wards);
         }
         fetchItems();
     }, []);
+
+    useEffect(() => {
+        async function fetchItems() {
+
+            const wardDetails = await getWardsDetails();
+            setWardDetails(wardDetails);
+        }
+        fetchItems();
+    }, [values.wardName]);
 
     return (
         <DashboardLayout>
@@ -152,35 +194,61 @@ function EvaluateStudents() {
                                 <MDBox component="form" role="form">
                                     <MDBox display="flex" justifyContent="center" alignItems="center">
                                         <MDBox m={1} pb={2} width={"25%"}>
-                                            <MDInput
-                                                error={!!errors.wardName}
-                                                type="text"
-                                                label="Ward Name"
-                                                name="wardName"
-                                                fullWidth
-                                                value={values.wardName}
-                                                onChange={handleChange}
-                                            />
+                                            <FormControl fullWidth variant="outlined" error={!!errors.classTeacher}>
+                                                <InputLabel id="user-role-select-label">Select ward</InputLabel>
+                                                <Select
+                                                    labelId="user-role-select-label"
+                                                    id="user-role-select"
+                                                    name="wardName"
+                                                    label="Select ward"
+                                                    sx={{ height: "44px;" }}
+                                                    value={values.wardName}
+                                                    onChange={handleChange}
+                                                >
+                                                    {items.map((item, index) => (
+                                                        <MenuItem key={index} value={item.wardDTO.wardName}>{item.wardDTO.wardName}</MenuItem>
+                                                    ))}
+                                                </Select>
+                                            </FormControl>
                                         </MDBox>
                                         <MDBox m={1} pb={2} width={"25%"}>
+                                            <FormControl fullWidth variant="outlined" error={!!errors.classTeacher}>
+                                                <InputLabel id="user-role-select-label">Select Student</InputLabel>
+                                                <Select
+                                                    labelId="user-role-select-label"
+                                                    id="user-role-select"
+                                                    name="student"
+                                                    label="Select student"
+                                                    sx={{ height: "44px;" }}
+                                                    // value={ }
+                                                    onChange={handleChange}
+                                                >
+                                                    {/* {wardDetails && wardDetails[0].students.map((student, index) => (
+                                                        <MenuItem key={index} value={student.userDTO.email}>{student.userDTO.email}</MenuItem>
+                                                    ))} */}
+                                                </Select>
+                                            </FormControl>
+                                        </MDBox>
+
+                                        <MDBox m={1} pb={2} width={"25%"}>
                                             <MDInput
-                                                error={!!errors.wardName}
+                                                error={!!errors.sportData}
                                                 type="text"
                                                 label="Sport Data"
                                                 name="sportData"
                                                 fullWidth
-                                                value={values.wardName}
+                                                value={values.sportData}
                                                 onChange={handleChange}
                                             />
                                         </MDBox>
                                         <MDBox m={1} pb={2} width={"25%"}>
                                             <MDInput
-                                                error={!!errors.wardName}
+                                                error={!!errors.extracurricularActivityGrade}
                                                 type="text"
                                                 label="Extracurricular Activity Grade"
                                                 name="extracurricularActivityGrade"
                                                 fullWidth
-                                                value={values.wardName}
+                                                value={values.extracurricularActivityGrade}
                                                 onChange={handleChange}
                                             />
                                         </MDBox>
@@ -188,85 +256,84 @@ function EvaluateStudents() {
                                     <MDBox display="flex" justifyContent="center" alignItems="center">
                                         <MDBox m={1} pb={2} width={"25%"}>
                                             <MDInput
-                                                error={!!errors.wardName}
+                                                error={!!errors.evaluationMonth}
                                                 type="text"
                                                 label="Evaluation Month"
                                                 name="evaluationMonth"
                                                 fullWidth
-                                                value={values.wardName}
+                                                value={values.evaluationMonth}
                                                 onChange={handleChange}
                                             />
                                         </MDBox>
                                         <MDBox m={1} pb={2} width={"25%"}>
                                             <MDInput
-                                                error={!!errors.wardName}
+                                                error={!!errors.evaluationYear}
                                                 type="text"
                                                 label="Evaluation Year"
                                                 name="evaluationYear"
                                                 fullWidth
-                                                value={values.wardName}
+                                                value={values.evaluationYear}
                                                 onChange={handleChange}
                                             />
                                         </MDBox>
                                         <MDBox m={1} pb={2} width={"25%"}>
                                             <MDInput
-                                                error={!!errors.wardName}
+                                                error={!!errors.behavioralData}
                                                 type="text"
                                                 label="Behavioral Data"
                                                 name="behavioralData"
                                                 fullWidth
-                                                value={values.wardName}
-                                                onChange={handleChange}
-                                            />
-                                        </MDBox>
-                                    </MDBox>
-                                    <MDBox display="flex" justifyContent="center" alignItems="center">
-                                        <MDBox m={1} pb={2} width={"25%"}>
-                                            <MDInput
-                                                error={!!errors.wardName}
-                                                type="text"
-                                                label="Extra Note"
-                                                name="extraNote"
-                                                fullWidth
-                                                value={values.wardName}
+                                                value={values.behavioralData}
                                                 onChange={handleChange}
                                             />
                                         </MDBox>
                                         <MDBox m={1} pb={2} width={"25%"}>
                                             <MDInput
-                                                error={!!errors.wardName}
-                                                type="text"
-                                                label="Extracurricular Activities"
-                                                name="extracurricularActivities"
-                                                fullWidth
-                                                value={values.wardName}
-                                                onChange={handleChange}
-                                            />
-                                        </MDBox>
-                                        <MDBox m={1} pb={2} width={"25%"}>
-                                            <MDInput
-                                                error={!!errors.wardName}
-                                                type="text"
-                                                label="Health Data"
-                                                name="healthData"
-                                                fullWidth
-                                                value={values.wardName}
-                                                onChange={handleChange}
-                                            />
-                                        </MDBox>
-                                    </MDBox>
-                                    <MDBox display="flex" justifyContent="center" alignItems="center">
-                                        <MDBox m={1} pb={2} width={"25%"}>
-                                            <MDInput
-                                                error={!!errors.wardName}
+                                                error={!!errors.sportGrade}
                                                 type="text"
                                                 label="Sport Grade"
                                                 name="sportGrade"
                                                 fullWidth
-                                                value={values.wardName}
+                                                value={values.sportGrade}
                                                 onChange={handleChange}
                                             />
                                         </MDBox>
+                                    </MDBox>
+                                    <MDBox display="flex" justifyContent="center" alignItems="center">
+                                        <MDBox m={1} pb={2} width={"25%"}>
+                                            <MDInput
+                                                error={!!errors.extraNote}
+                                                type="text"
+                                                label="Extra Note"
+                                                name="extraNote"
+                                                fullWidth
+                                                value={values.extraNote}
+                                                onChange={handleChange}
+                                            />
+                                        </MDBox>
+                                        <MDBox m={1} pb={2} width={"25%"}>
+                                            <MDInput
+                                                error={!!errors.extracurricularActivities}
+                                                type="text"
+                                                label="Extracurricular Activities"
+                                                name="extracurricularActivities"
+                                                fullWidth
+                                                value={values.extracurricularActivities}
+                                                onChange={handleChange}
+                                            />
+                                        </MDBox>
+                                        <MDBox m={1} pb={2} width={"25%"}>
+                                            <MDInput
+                                                error={!!errors.healthData}
+                                                type="text"
+                                                label="Health Data"
+                                                name="healthData"
+                                                fullWidth
+                                                value={values.healthData}
+                                                onChange={handleChange}
+                                            />
+                                        </MDBox>
+
                                     </MDBox>
                                     <MDBox m={1} pb={2} display="flex" justifyContent="end">
                                         <Button
